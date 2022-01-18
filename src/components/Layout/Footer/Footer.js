125041ab87a97a2541a4411215378308ce51efe9
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2022-01-18 12:03:48 +0300

    added nav menuIcon Material UI

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 import Instagram from '../../../images/instagram.svg';
 import Twitter from '../../../images/tw-3.svg';
-import Facebook from '../../../images/fb.svg';
+import Git from '../../../images/git.png';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import Aux from '../../../hoc/_Aux';
 
@@ -12,9 +12,9 @@ const Footer = (props) => (
         <div className="Icons">
             <a className="Instagram" target='_blank' rel="noreferrer" href='https://www.instagram.com/gulnazmd/'><img className="Inst" src={Instagram} alt="альтернативный текст"/></a>
             <img className="Twitter" src={Twitter} alt="альтернативный текст"/>
-            <img className="Facebook" src={Facebook} alt="альтернативный текст"/>
+            <a className='Git' target='_blank' rel='noreferrer' href='https://github.com/Gulnazmd'><img className='GitHab' src={Git} alt="альтернативный текст"/></a>
         </div>
-                    
+
     </div>
     <div className="Icon">
                         <small> © Copyright Gulnaz <FavoriteBorderIcon/>  </small>
--- src/components/Layout/Hearder/Hearder.js
+++ src/components/Layout/Hearder/Hearder.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Hearder.css';
 import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import { Box, IconButton, Menu, MenuItem } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Header = (props) => (
-    <div className="Header">
-       <ul>
-           <li><Link to="/">MaturDev</Link></li>
-           <li><Link to="/blogs">Blog</Link></li>
-           <li><Link to="/contact">Contact</Link></li>
-       </ul>
-       <p className='Sign'>
-           <Link opened={props.open} onClick={props.clicked} to="/dataform">Sign IN</Link>
-       </p>
-    </div>
-)
-export default Header;
+
+export default function Header (props) {
+
+        const[ anchorEl, setAnchorEl ] = useState(null);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+        return (
+            <div className="Header">
+                <Box>
+                    <AppBar position="static"
+                        color='grey'
+                        elevation={0}>
+                        <Toolbar>
+                            <IconButton
+                                size="large"
+                                edge="start"
+                                color="inherit"
+                                aria-label="menu"
+                                sx={{ mr: 2 }}>
+
+                                    <MenuIcon onClick={handleMenu} />
+                                     <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                      vertical: 'top',
+                                      horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                      vertical: 'top',
+                                      horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorEl)}
+                                    onClose={handleClose}>
+                                    <MenuItem className='MyMenu' onClick={handleClose}><Link to="/blogs">Blog</Link></MenuItem>
+                                    <MenuItem className='MyMenu' onClick={handleClose}><Link to="/contact">Contact</Link></MenuItem>
+                                </Menu>
+                            </IconButton>
+                            <Typography className='Type'><Link to="/">MaturDev</Link></Typography>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+                <p className='Sign'>
+                    <Link opened={props.open} onClick={props.clicked} to="/dataform">Sign IN</Link>
+                </p>
+            </div>
+        )}
